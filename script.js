5e0b60cf754ee7c9c2d6fdc33ada4eca3ea9b27b
Catarina007 <00001106359987sp@al.educacao.sp.gov.br>
2024-09-09 15:05:17 -0300

    .

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
 
 button.addEventListener('click', function(){
-    const atual - document.querySelector('ativo');
-    const proximoPasso 
+    const atual = document.querySelector('ativo');
+    const proximoPasso = 'passo-' + this.getAttribute
 })
 
 })
